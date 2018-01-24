7f38b9562907b223f1fa834e4bfbdf39542a6a61
VasiliuKr <vasil.krasnov@gmail.com>
2018-01-24 17:45:44 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-	console.log("hello");
+$(window).scroll(function() {
+// 80 = The point you would like to fade the nav in.
+	if ($(window).scrollTop() > 80 ){
+		$('.header').addClass('header_white');
+	} else {
+		$('.header').removeClass('header_white');
+	};
+});
+
+$('.scroll').on('click', function(e){
+	e.preventDefault()
+	$('html, body').animate({
+		scrollTop : $(this.hash).offset().top
+	}, 1500);
 });
