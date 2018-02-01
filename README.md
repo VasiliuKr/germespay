8a3d446a4475d794cc8c5d180c37861ae12c0e83
VasiliuKr <vasil.krasnov@gmail.com>
2018-02-01 16:45:56 +0300

    update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # germespay
-useful links
+**useful links**
+
 https://codepen.io/shaneheyns/pen/OPWGry  radio input change content
 
 https://codepen.io/soulrider911/pen/qhEzf  radio input style
@@ -7,3 +8,15 @@ https://codepen.io/soulrider911/pen/qhEzf  radio input style
 https://snazzymaps.com/style/132/light-gray gray style map
 
 https://codepen.io/wallaceerick/pen/ctsCz styled select
+
+**TODO**
+
+Отобразить на 1366px
+
+Добавить возможность писать в поле ввода, где стоит select (Город, Улица и пр.)
+
+Добавить валидацию инпутов на фронте при потери фокуса
+
+Добавить правую синюю границу у пунктов
+
+Работа с картой (клик по пункту - центрирование и приближение, возможно анимация выпадания меток на карте)
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+/********************* Fixed header (start) *********************/
 $(window).scroll(function() {
 // 80 = The point you would like to fade the nav in.
 	if ($(window).scrollTop() > 80 ){
@@ -13,8 +15,10 @@ $('.scroll').on('click', function(e){
 		scrollTop : $(this.hash).offset().top
 	}, 1500);
 });
+/********************* Fixed header (end) *********************/
 
 
+/*********************** Form inputs (start) **********************/
 $(document).on('focus', '.payment-form__input', function() {
 	if( $(this).val() === "" ){
 		$(this).parent().find('label').addClass('active');
@@ -25,3 +29,64 @@ $(document).on('blur', '.payment-form__input', function() {
 		$(this).parent().find('label').removeClass('active');
 	}
 });
+/*********************** Form inputs (end) **********************/
+
+
+/*********************** Styled select (start) **********************/
+$('select').each(function(){
+	var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+	$this.addClass('select-hidden'); 
+	$this.wrap('<div class="select"></div>');
+	$this.after('<div class="select-styled"></div>');
+
+	var $styledSelect = $this.next('div.select-styled');
+	$styledSelect.text($this.children('option').eq(0).text());
+
+	var $list = $('<ul />', {
+		'class': 'select-options'
+	}).insertAfter($styledSelect);
+
+	for (var i = 0; i < numberOfOptions; i++) {
+		$('<li />', {
+			text: $this.children('option').eq(i).text(),
+			rel: $this.children('option').eq(i).val()
+		}).appendTo($list);
+	}
+
+	var $listItems = $list.children('li');
+
+	$styledSelect.click(function(e) {
+		e.stopPropagation();
+		$('div.select-styled.active').not(this).each(function(){
+			$(this).removeClass('active').next('ul.select-options').hide();
+		});
+		$(this).toggleClass('active').next('ul.select-options').toggle();
+	});
+
+	$listItems.click(function(e) {
+		e.stopPropagation();
+		$styledSelect.text($(this).text()).removeClass('active');
+		$styledSelect.text($(this).text()).addClass('selected');
+		$this.val($(this).attr('rel'));
+		$list.hide();
+		// console.log($this.val());
+	});
+
+	$(document).click(function() {
+		$styledSelect.removeClass('active');
+		$list.hide();
+	});
+
+});
+/*********************** Styled select (end) **********************/
+
+/***************** Changing form content (start) ******************/
+$(document).on('change', '.payment-modal__radio', function(event) {
+	// event.preventDefault();
+	var addressForm = $('#paymentByAddress'),
+		accountForm = $('#paymentByAccount');
+	addressForm.toggle();
+	accountForm.toggle();	
+});
+/***************** Changing form content (end) ******************/
